07f489d82970390d12307950ce403df0d368e3e4
ri-tae-ka <ritzthareja@gmail.com>
2022-04-30 10:23:08 +0530

    langing page images to png

--- frontend/src/components/landingPage/CanteenNames.js
+++ frontend/src/components/landingPage/CanteenNames.js
@@ -1,16 +1,15 @@
-import React from 'react'
-import './CanteenNames.css'
-import {Link} from "react-router-dom"
+import React from "react";
+import "./CanteenNames.css";
+import { Link } from "react-router-dom";
 
 export default function CanteenNames() {
   return (
-    <div className = "canteen-bar">
-      
-        <ul className='canteen-list'>
-          <li className='canteen-name'><Link to="/mukteshwari" style={{ textDecoration: "none", color: "white" }}>Mukteshwari</Link></li>
-          
-        </ul>
-    
+    <div className="canteen-bar">
+      <ul className="canteen-list">
+        <li className="canteen-name">
+          <Link to="/mukteshwari">Mukteshwari</Link>
+        </li>
+      </ul>
     </div>
-  )
+  );
 }
