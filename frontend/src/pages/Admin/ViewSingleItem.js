3442345ee10ab44cdeefb2fc4d4259d2a9c3de31
khushboo <goyalq000@gmail.com>
2022-05-04 15:07:05 +0530

    Viwe single user

--- frontend/src/pages/Admin/ViewSingleItem.js
+++ frontend/src/pages/Admin/ViewSingleItem.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import './ViewSingleItem.css';
+import { NavLink } from "react-router-dom";
 
 function ViewSingleItem() {
 
@@ -36,17 +38,25 @@ function ViewSingleItem() {
 
   return (
     <>
-    
-      <div>
-        <img src={getFoodDetails.food_image} alt=""/>
-      </div>
-      <div>
+  <div class = "box">
+    <h1>Food Item Details</h1>
+    <div class="card_wrapper">
+    <div class="column">
+    <div class="image_wrapper">
+      <div class="lense">
+        <img src={getFoodDetails.food_image} alt="" />
+    </div>
+    </div>
+    </div>
+    </div>
+      <div class = "details">
         <h2>{getFoodDetails.food_name}</h2>
-        <p>{getFoodDetails.food_description}</p>
-        <h4>Price - {getFoodDetails.food_price}</h4>
-        <h4>Quantity - {getFoodDetails.food_quantity}</h4>
-        <h4>Food Category - {getFoodDetails.food_itemId}</h4>
+        <div class="price"><h4>Price - {getFoodDetails.food_price}</h4></div>
+        <div class="quantity"><h4>Quantity - {getFoodDetails.food_quantity}</h4></div>
+        <div class="foodcategory"><h4>Food Category - {getFoodDetails.food_itemId}</h4></div>
       </div>
+      <div class = "p"><p>{getFoodDetails.food_description}</p></div>
+    </div>
     </>
   )
 }
